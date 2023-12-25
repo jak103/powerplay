eca47e3f5141c6519a1a08368ce5734cc0d4c066
Jacob Christensen <jacob.h.christensen@gmail.com>
2023-12-24 22:34:59 -0700

    initial layout buttons and icons

--- app/src/layouts/MainLayout.vue
+++ app/src/layouts/MainLayout.vue
@@ -1,102 +1,137 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="lHh Lpr fff" class="bg-grey-1">
+    <q-header elevated class="bg-white text-grey-8" height-hint="64">
+      <q-toolbar class="GPL__toolbar" style="height: 64px">
 
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title shrink class="row items-center no-wrap">
+          <span class="q-ml-sm">League Manager</span>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+
+        <q-select v-model="roleModel" :options="roleOptions" label="Standard" style="width: 200px" label-color="red" /> <!-- TODO Only show this in dev -->
+
+        <q-space />
+
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container class="GPL__page-container">
       <router-view />
+
+      <q-page-sticky v-if="$q.screen.gt.sm" expand position="left">
+        <div class="fit q-pt-xl q-px-sm column">
+          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
+            <q-icon name="sports_hockey" />
+            <div class="GPL__side-btn__label">Teams</div>
+          </q-btn>
+
+          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
+            <q-icon name="chat" />
+            <div class="GPL__side-btn__label">Chat</div>
+            <q-badge color="red" text-color="white" floating style="top: 8px; right: 16px">
+              2
+            </q-badge>
+          </q-btn>
+
+          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
+            <q-icon name="calendar_month" />
+            <div class="GPL__side-btn__label">Schedule</div>
+            <q-badge floating color="red" text-color="white" style="top: 8px; right: 16px">
+              1
+            </q-badge>
+          </q-btn>
+
+          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
+            <q-icon name="paid" />
+            <div class="GPL__side-btn__label">Finance</div>
+          </q-btn>
+
+          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
+            <q-icon name="account_circle" />
+            <div class="GPL__side-btn__label">Account</div>
+          </q-btn>
+
+          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
+            <q-icon name="settings" />
+            <div class="GPL__side-btn__label">Admin</div>
+          </q-btn>
+        </div>
+      </q-page-sticky>
+
+
+      <q-footer v-if="$q.screen.lt.md" class="">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
+
 import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+const roleModel = ref(null);
+const roleOptions = ['Player', 'Captain', 'Staff', 'Manager']
 
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
 </script>
+
+<style lang="sass">
+.GPL
+
+  &__toolbar
+    height: 64px
+
+  &__toolbar-input
+    width: 35%
+
+  &__drawer-item
+    line-height: 24px
+    border-radius: 0 24px 24px 0
+    margin-right: 12px
+
+    .q-item__section--avatar
+      padding-left: 12px
+      .q-icon
+        color: #5f6368
+
+    .q-item__label:not(.q-item__label--caption)
+      color: #3c4043
+      letter-spacing: .01785714em
+      font-size: .875rem
+      font-weight: 500
+      line-height: 1.25remns
+
+    &--storage
+      border-radius: 0
+      margin-right: 0
+      padding-top: 24px
+      padding-bottom: 24px
+
+  &__side-btn
+    &__label
+      font-size: 12px
+      line-height: 24px
+      letter-spacing: .01785714em
+      font-weight: 500
+
+  @media (min-width: 1024px)
+    &__page-container
+      padding-left: 94px
+</style>
