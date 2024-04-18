4cb7ade906f5129e1c29d2f4574dcf60a6c144e6
Nathan Blaylock <ngblaylock@gmail.com>
2024-04-17 22:24:23 -0600

    Update theme init file

--- frontend/app.vue
+++ frontend/app.vue
@@ -5,7 +5,7 @@ useHead({
       src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
     },
     {
-      src: '/theme.js',
+      src: '/initTheme.js',
       tagPosition: 'bodyOpen'
     }
   ],
--- frontend/public/initTheme.js
+++ frontend/public/initTheme.js
@@ -1,3 +1,5 @@
+// This will initialize the theme without a quick flash of light theme, then changing to dark theme on the first load.
+
 const theme = localStorage.getItem('theme');
 if(theme === 'dark'){
   document.body.setAttribute('data-bs-theme', 'dark')
