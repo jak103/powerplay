c5c7944ee00105f9c6d1198293c1289cdb796041
Michael Holmes <a02395591@aggies.usu.edu>
2024-05-14 16:47:26 -0600

    Testing PWA resource caching

--- frontend/app.vue
+++ frontend/app.vue
@@ -18,6 +18,18 @@ useHead({
   titleTemplate: (title) =>
     title ? `${title} | Eccles Ice Hockey` : "Eccles Ice Hockey",
 });
+
+// Register service worker
+onMounted(async () => {
+        if ('serviceWorker' in navigator) {
+          try {
+            const registration = await navigator.serviceWorker.register('/sw.js');
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          } catch (err) {
+            console.log('ServiceWorker registration failed: ', err);
+          }
+        }
+      });
 </script>
 
 <template>
