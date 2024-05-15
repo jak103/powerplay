200e202aa647452ae3d05d4833903e29e46a6ecd
Michael Holmes <michael.holmesucas@gmail.com>
2024-05-14 20:16:19 -0600

    Included only urls containing 'app' for the PWA app, as well as caching styling files in the PWA

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -28,19 +28,16 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        'api/docs.vue',
-        '/app/chat/index.vue',
-        '/app/profile/index.vue',
-        '/app/schedule/index.vue',
-        '/app/index.vue',
-        '/app/create-account.vue',
-        '/app/sign-in.vue',
-        'app.vue',
-        'calendar.vue',
-        'index.vue',
-        'information/index.vue',
-        'information/managers.vue',
-        'sw.js',
+        '/app',
+        '/app/chat',
+        '/app/profile',
+        '/app/schedule',
+        '/app/create-account',
+        '/app/sign-in',
+        '/_nuxt/layouts/app-layout.vue?vue&type=style&index=0&scoped=031fc290&lang.scss',
+        '/_nuxt/node_modules/nuxt/dist/app/plugins/check-if-layout-used.js?v=f43cdfe7',
+        '/_nuxt/@id/virtual:nuxt:C:/Users/micha/Homework/5890/Powerplay/frontend/.nuxt/layouts.mjs',
+        '/_nuxt/node_modules/nuxt/dist/app/components/nuxt-layout.js?v=f43cdfe7',
         // Add other assets as needed
       ]);
     })
@@ -48,23 +45,21 @@ self.addEventListener('install', (event) => {
 });
 
 
-// Load resources from cache if possible, otherwise fetch them from the network (faster to load from cache)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cacheResponse) => {
-      // If the resource is in the cache, return it
-      if (cacheResponse) {
-        return cacheResponse;
-      }
-
-      // If the resource is not in the cache, fetch it from the network, cache it, and return it
-      return fetch(event.request).then((networkResponse) => {
-        return caches.open('v1').then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      }).catch(() => {
-        // If the network is unavailable and the resource is not in the cache, return a fallback response
+    fetch(event.request).then((networkResponse) => {
+      return caches.open('v1').then((cache) => {
+        // Cache the response for future use
+        cache.put(event.request, networkResponse.clone());
+        return networkResponse;
+      });
+    }).catch(() => {
+      // If the network is unavailable, try to serve the resource from the cache
+      return caches.match(event.request).then((cacheResponse) => {
+        if (cacheResponse) {
+          return cacheResponse;
+        }
+        // If the resource is not in the cache, return a fallback response
         return new Response('Offline', { status: 503 });
       });
     })
