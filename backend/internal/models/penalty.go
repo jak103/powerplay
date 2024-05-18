e29fcbb8f3dd5546d9fe5b107308552272bb549f
Ryan Barlow <barlow.ryan@rocketmail.com>
2024-05-17 18:38:53 -0600

    Added Penalty model

--- backend/internal/models/penalty.go
+++ backend/internal/models/penalty.go
@@ -10,3 +10,18 @@ type PenaltyType struct {
 	Duration time.Duration `json:"duration"`
 	Severity string        `json:"severity"`
 }
+
+type Penalty struct {
+	DbModel
+	PlayerID      uint        `json:"player_id"`
+	Player        User        `json:"player"`
+	TeamID        uint        `json:"team_id"`
+	Team          Team        `json:"team"`
+	GameID        uint        `json:"game_id"`
+	Game          Game        `json:"game"`
+	Period        uint        `json:"period"` // TODO: how to represent Shootouts?
+	Duration      uint        `json:"duration"`
+	CreatedBy     uint        `json:"created_by"` // TODO: Should this be in the model.go DbModel?
+	PenaltyTypeID uint        `json:"penalty_type_id"`
+	PenaltyType   PenaltyType `json:"penalty_type"`
+}
