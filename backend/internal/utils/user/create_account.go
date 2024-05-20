77b31955f337b8342b1e81fddb7c9882da35e1b6
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-05-20 05:22:45 -0600

    Updated validate user and added removeFormat

--- backend/internal/utils/user/create_account.go
+++ backend/internal/utils/user/create_account.go
@@ -2,7 +2,12 @@ package user
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/mail"
+	"reflect"
+	"regexp"
+	"strconv"
 )
 
 type User struct {
@@ -24,8 +29,14 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
-
-	validateUser(user)
+	//Remove non-Alphanumerics like () and - from phone number
+	user.Phone = removeFormat(user.Phone)
+	//Check if data is valid
+	err = validateUser(user)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Validation error: %s", err), http.StatusBadRequest)
+		return
+	}
 
 	// Validation passed, we can now create the account here
 
@@ -33,5 +44,30 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func validateUser(u User) {
+func removeFormat(str string) string {
+	return regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(str, "")
+}
+
+func validateUser(u User) error {
+	values := reflect.ValueOf(u)
+	for i := 0; i < values.NumField(); i++ {
+		v := values.Field(i).String()
+		//Check data field has been filled for all values
+		if v == "" {
+			return fmt.Errorf("data field is empty")
+		}
+	}
+	//Validate email has an @ in middle
+	if _, err := mail.ParseAddress(u.Email); err != nil {
+		return fmt.Errorf("%s is an invalid Email", u.Email)
+	}
+	//Validate phone number is 10 digit int
+	if _, err := strconv.Atoi(u.Phone); err != nil || len(u.Phone) != 10 {
+		return fmt.Errorf("%s is an invalid phone number", u.Phone)
+	}
+	//Validate skill level is an at least 0
+	if u.SkillLevel < 0 {
+		return fmt.Errorf("skill level must be positive, %d is negative", u.SkillLevel)
+	}
+	return nil
 }
