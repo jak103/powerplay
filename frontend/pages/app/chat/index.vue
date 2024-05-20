13dbb777dcd67a631691d1af74e88c7c58e439e0
Gavin Robey <gavin.robey15@gmail.com>
2024-05-20 10:31:58 -0600

    added ability for components to render data from fake json

--- frontend/pages/app/chat/index.vue
+++ frontend/pages/app/chat/index.vue
@@ -3,45 +3,97 @@
   import AppHeader from '~/components/AppHeader.vue';
   import Card from '~/components/Card.vue';
 
+  const theme = ref<null | string>(null);
+  onMounted(() => {
+    theme.value = localStorage.getItem("theme") || "light";
+  });
+
 
   // TODO: pull data in this form from the backend
   const data = {
     channels : [
       {
-        "channel_name": "Toronto Maple Leafs",
-        "channel_id": "mapleleafs123",
+        "channel_name": "Trash Pandas",
+        "channel_id": "example1",
         "channel_image": "mapleleafs_image.jpg",
-        "missed_chats": 5,
-        "number_of_members": 100
-      },
-      {
-        "channel_name": "Montreal Canadiens",
-        "channel_id": "canadiens456",
-        "channel_image": "canadiens_image.jpg",
-        "missed_chats": 2,
-        "number_of_members": 50
-      },
-      {
-        "channel_name": "Edmonton Oilers",
-        "channel_id": "oilers789",
-        "channel_image": "oilers_image.jpg",
-        "missed_chats": 10,
-        "number_of_members": 200
-      },
-      {
-        "channel_name": "Vancouver Canucks",
-        "channel_id": "canucks101",
-        "channel_image": "canucks_image.jpg",
         "missed_chats": 1,
-        "number_of_members": 30
+        "type" : "team"
+        
+      },
+      {
+        "channel_name": "District 5",
+        "channel_id": "example2",
+        "channel_image": "canadiens_image.jpg",
+        "missed_chats": 16,
+        "type" : "team"
+      },
+      {
+        "channel_name": "C-League",
+        "channel_id": "example3",
+        "channel_image": null,
+        "missed_chats": 0,
+        "type" : "league"
+      },
+      {
+        "channel_name": "D-League",
+        "channel_id": "example4",
+        "channel_image": null,
+        "missed_chats": 0,
+        "type" : "league"
+      },
+      {
+        "channel_name": "C-League Subs",
+        "channel_id": "example5",
+        "channel_image": null,
+        "missed_chats": 3,
+        "type" : "channel"
+      },
+      {
+        "channel_name": "D-League Subs",
+        "channel_id": "example6",
+        "channel_image": null,
+        "missed_chats": 0,
+        "type" : "channel"
+      },
+      {
+        "channel_name": "News & Announcements",
+        "channel_id": "example7",
+        "channel_image": null,
+        "missed_chats": 1,
+        "type" : "channel"
+      },
+      {
+        "channel_name": "Corey Truetel",
+        "channel_id": "example8",
+        "channel_image": "oilers_image.jpg",
+        "missed_chats": 0,
+        "type" : "dm"
+      },
+      {
+        "channel_name": "D-League Subs",
+        "channel_id": "example9",
+        "channel_image": "canucks_image.jpg",
+        "missed_chats": 3,
+        "type" : "dm"
+      },
+      {
+        "channel_name": "Scott Pococha",
+        "channel_id": "example10",
+        "channel_image": "canucks_image.jpg",
+        "missed_chats": 0,
+        "type" : "dm"
       }
     ]
   }
 
   const createChannel = "/app/chat/create-channel"
   const chatRoom = "/app/chat/chat-room"
-  const name = "example"
   const chat = "Chat"
+
+  const teams = data.channels.filter(channel => channel.type === "team");
+  const leagues = data.channels.filter(channel => channel.type === "league");
+  const channels = data.channels.filter(channel => channel.type === "channel");
+  const dms = data.channels.filter(channel => channel.type === "dm");
 
 </script>
 
@@ -51,36 +103,30 @@
   <div id="container">
     <div>
       <h2>Teams</h2>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
+      <Card v-for="channel in teams" :key="channel.channel_id" :type="channel.type"
+      :name="channel.channel_name" :link="chatRoom" :unread="channel.missed_chats"/>
       <div class="line"></div>
-
       <br>
       
       <h2>Leagues</h2>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
+      <Card v-for="channel in leagues" :key="channel.channel_id" :type="channel.type"
+      :name="channel.channel_name" :link="chatRoom" :unread="channel.missed_chats"/>
       <div class="line"></div>
-
       <br>
 
       <h2>Channels</h2>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
+      <Card v-for="channel in channels" :key="channel.channel_id" :type="channel.type"
+      :name="channel.channel_name" :link="chatRoom" :unread="channel.missed_chats"/>
       <div class="line"></div>
-
       <br>
 
       <h2>Direct Messages</h2>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
-      <Card :name="name" :link="chatRoom"/>
+      <Card v-for="channel in dms" :key="channel.channel_id" :type="channel.type"
+      :name="channel.channel_name" :link="chatRoom" :unread="channel.missed_chats"/>
       <div class="line"></div>
+      <br>
+      <br>
+      <br>
     </div>
     <FloatingButton :link="createChannel"/>
   </div>
