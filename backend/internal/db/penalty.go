d06f521f9a76f5a140907cfb41ed81993fb1d3be
Ryan Barlow <barlow.ryan@rocketmail.com>
2024-05-20 19:35:01 -0600

    Added preloading for PenaltyType

--- backend/internal/db/penalty.go
+++ backend/internal/db/penalty.go
@@ -4,7 +4,7 @@ import "github.com/jak103/powerplay/internal/models"
 
 func (s session) GetPenalties() ([]models.Penalty, error) {
 	penalties := make([]models.Penalty, 0)
-	err := s.connection.Find(&penalties)
+	err := s.connection.Preload("PenaltyType").Find(&penalties)
 	return resultsOrError(penalties, err)
 }
 
