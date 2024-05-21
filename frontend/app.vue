c509225143bc5ba8620076936881eb50945d000a
Michael Holmes <michael.holmesucas@gmail.com>
2024-05-21 17:50:24 -0600

    Enables text selection on computers. Text selection still disabled on touchscreen devices

--- frontend/app.vue
+++ frontend/app.vue
@@ -40,11 +40,14 @@ onMounted(async () => {
 </template>
 
 <style>
-  body {
-    /* Prevent text selection */
-    -webkit-user-select: none; /* Safari */
-    -moz-user-select: none; /* Firefox */
-    -ms-user-select: none; /* Internet Explorer/Edge */
-    user-select: none; /* Non-prefixed version, currently supported by Chrome, Opera, and Edge */
+  /* Disable text selection on touchscreen devices */
+  @media (hover: none) { 
+    body {
+      /* Prevent text selection */
+      -webkit-user-select: none; /* Safari */
+      -moz-user-select: none; /* Firefox */
+      -ms-user-select: none; /* Internet Explorer/Edge */
+      user-select: none; /* Non-prefixed version, currently supported by Chrome, Opera, and Edge */
+    }
   }
 </style>
