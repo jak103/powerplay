ce66d30089ebd92d2705402003547307508b997b
Aryan Osqueezadeh <A02289878@aggies.usu.edu>
2024-05-29 12:32:47 -0600

    Added Cypress folders and example

---
+++ app/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      console.log(on); // Temporarily prevents lint errors
+      console.log(config); // Temporarily prevents lint errors
+      // TODO: implement node event listeners here
+    },
+  },
+});
