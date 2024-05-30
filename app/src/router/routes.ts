c0b34a5442c475ba9b53a7776a38091c49bbc90c
mattlemone <lemonbaseball@gmail.com>
2024-05-30 08:23:10 -0600

    edited paths to allow for the same layout to be there

--- app/src/router/routes.ts
+++ app/src/router/routes.ts
@@ -4,17 +4,11 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/HomePage.vue') }],
-  },
-  {
-    path: '/TeamInfo/:teamName',
-    name: 'TeamInfo',
-    component: () => import('pages/TeamInfo.vue')
-  },
-  {
-    path: '/LeagueInfo/:id',
-    name: 'LeagueInfo',
-    component: () => import('pages/LeagueInfo.vue')
+    children: [
+    { path: '', component: () => import('pages/HomePage.vue') },
+    { path: 'teaminfo/:teamName', name: 'TeamInfo', component: () => import('pages/TeamInfo.vue') },
+    { path: 'leagueinfo/:id', name: 'LeagueInfo', component: () => import('pages/LeagueInfo.vue') },
+  ],
   },
 
   // Always leave this as last one,
