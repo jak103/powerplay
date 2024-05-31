130d64f9f74619b4dc2a309332be685fdc590185
Samuelk-Chase <samuelkchase@icloud.com>
2024-05-30 18:50:17 -0600

    got the other two api's up, get game by ID and update by ID now show up

--- backend/internal/db/game.go
+++ backend/internal/db/game.go
@@ -2,13 +2,18 @@ package db
 
 import (
 	"github.com/jak103/powerplay/internal/models"
+	"github.com/jak103/powerplay/internal/utils/log"
 )
 
 // GetGames retrieves all games from the database.
-func (s session) GetGames() ([]models.League, error) {
-	games := make([]models.League, 0)
-	err := s.connection.Find(&games)
-	return resultsOrError(games, err)
+func (s *session) GetGames() ([]models.Game, error) {
+	games := make([]models.Game, 0)
+	err := s.connection.Find(&games).Error
+	if err != nil {
+		log.WithErr(err).Alert("Failed to retrieve games from database")
+		return nil, err
+	}
+	return games, nil
 }
 
 // GetGameByID retrieves a game by its ID from the database.
@@ -16,6 +21,7 @@ func (s *session) GetGameByID(gameID string) (*models.Game, error) {
 	var game models.Game
 	err := s.connection.First(&game, "id = ?", gameID).Error
 	if err != nil {
+		log.WithErr(err).Alert("Failed to retrieve game from database")
 		return nil, err
 	}
 	return &game, nil
@@ -26,14 +32,21 @@ func (s *session) UpdateGame(gameID string, game models.Game) error {
 	var existingGame models.Game
 	err := s.connection.First(&existingGame, "id = ?", gameID).Error
 	if err != nil {
+		log.WithErr(err).Alert("Failed to find existing game in database")
 		return err
 	}
 	err = s.connection.Model(&existingGame).Updates(game).Error
+	if err != nil {
+		log.WithErr(err).Alert("Failed to update game in database")
+	}
 	return err
 }
 
 // CreateGame creates a new game in the database.
 func (s *session) CreateGame(game *models.Game) error {
 	err := s.connection.Create(game).Error
+	if err != nil {
+		log.WithErr(err).Alert("Failed to create game in database")
+	}
 	return err
 }
