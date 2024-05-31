40a033a26b61c7288c489d2482b36d23fb2cb5ec
zanehirning <a02371568@usu.edu>
2024-05-30 23:37:00 -0600

    fix some build errors

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -16,7 +16,6 @@ import (
 	_ "github.com/jak103/powerplay/internal/server/apis/notifications"
 	_ "github.com/jak103/powerplay/internal/server/apis/stats"
 	_ "github.com/jak103/powerplay/internal/server/apis/user"
-    _ "github.com/jak103/powerplay/internal/server/apis/schedule"
 )
 
 func Run() {
