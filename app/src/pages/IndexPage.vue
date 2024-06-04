b3fdeccc8eb32365cf1f27b965271bebfe2ec8f4
Gavin Robey <gavin.robey15@gmail.com>
2024-06-04 15:28:33 -0600

    fixed string formatting

--- app/src/pages/IndexPage.vue
+++ app/src/pages/IndexPage.vue
@@ -1,67 +1,67 @@
 <template>
-  <q-page class="q-pa-md">
+  <q-page class='q-pa-md'>
     
-    <div class="text-h6">Upcoming Events</div>
+    <div class='text-h6'>Upcoming Events</div>
 
-    <div class="q-pa-md">
-      <q-card class="q-mb-md">
+    <div class='q-pa-md'>
+      <q-card class='q-mb-md'>
         <q-card-section>
           <q-item>
-            <q-avatar size="50px" class="q-mr-md">
-              <img src="path/to/logo.png" alt="Logo">
+            <q-avatar size='50px' class='q-mr-md'>
+              <img src='path/to/logo.png' alt='Logo'>
             </q-avatar>
             <q-item-section>
               <q-item-label>
-                <div class="text-h5">{{ game[0].hometeam }}</div>
-                <div class="text-subtitle2">Home</div>
+                <div class='text-h5'>{{ game[0].hometeam }}</div>
+                <div class='text-subtitle2'>Home</div>
               </q-item-label>
             </q-item-section>
           </q-item>
           <q-item>
-            <q-avatar size="50px" class="q-mr-md">
-              <img src="path/to/logo.png" alt="Logo">
+            <q-avatar size='50px' class='q-mr-md'>
+              <img src='path/to/logo.png' alt='Logo'>
             </q-avatar>
             <q-item-section>
               <q-item-label>
-                <div class="text-h5">{{ game[0].awayteam }}</div>
-                <div class="text-subtitle2">Locker Room 2</div>
+                <div class='text-h5'>{{ game[0].awayteam }}</div>
+                <div class='text-subtitle2'>Locker Room 2</div>
               </q-item-label>
             </q-item-section>
           </q-item>
-          <div class="text-subtitle2 q-ml-md">{{ game[0].date }}</div>
-          <div class="text-subtitle2 q-ml-md">{{ game[0].time }}</div>
+          <div class='text-subtitle2 q-ml-md'>{{ game[0].date }}</div>
+          <div class='text-subtitle2 q-ml-md'>{{ game[0].time }}</div>
         </q-card-section>
       </q-card>
     </div>
-    <div class="text-h6">My Teams</div>
+    <div class='text-h6'>My Teams</div>
 
-    <q-item v-for="team in teams" :key="team.id" class="q-mb-sm border-bottom">
-        <q-item clickable v-ripple @click="goToTeamInfo(team.name)">
-            <q-avatar size="30px" class="q-mr-md">
-                <img src="team.logo" alt="Logo">
+    <q-item v-for='team in teams' :key='team.id' class='q-mb-sm border-bottom'>
+        <q-item clickable v-ripple @click='goToTeamInfo(team.name)'>
+            <q-avatar size='30px' class='q-mr-md'>
+                <img src='team.logo' alt='Logo'>
             </q-avatar>
             <q-item-section>
-                <div class="text-h6 text-weight-regular">{{ team.name }}</div>
+                <div class='text-h6 text-weight-regular'>{{ team.name }}</div>
             </q-item-section>
         </q-item>
     </q-item>
 
-    <div class="text-h6">My Leagues</div>
+    <div class='text-h6'>My Leagues</div>
 
-    <q-item v-for="team in teams" :key="team.id" class="q-mb-sm border-bottom">
-        <q-item clickable v-ripple @click="goToLeagueInfo(team.league)">
-            <q-avatar size="30px" class="q-mr-md">
-                <img src="team.logo" alt="Logo">
+    <q-item v-for='team in teams' :key='team.id' class='q-mb-sm border-bottom'>
+        <q-item clickable v-ripple @click='goToLeagueInfo(team.league)'>
+            <q-avatar size='30px' class='q-mr-md'>
+                <img src='team.logo' alt='Logo'>
             </q-avatar>
             <q-item-section>
-                <div class="text-h6 text-weight-regular">{{ team.league }} League</div>
+                <div class='text-h6 text-weight-regular'>{{ team.league }} League</div>
             </q-item-section>
         </q-item>
     </q-item>
   </q-page>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
@@ -98,12 +98,12 @@ const game = ref([
 
 const goToTeamInfo = (teamName: string) => {
   const encodedTeamName = encodeURIComponent(teamName);
-  console.log("Going to team info");
+  console.log('Going to team info');
   router.push({ name: 'TeamInfo', params: { teamName: encodedTeamName } });
 };
 
 const goToLeagueInfo = (league: string) => {
-    console.log("Going to league info");
+    console.log('Going to league info');
   router.push({ name: 'LeagueInfo', params: { id: league } });
 };
 
