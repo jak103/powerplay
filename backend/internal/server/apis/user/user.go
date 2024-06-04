a16dee98228343b5498f17c3a6a393e1cb3029b0
Bryson Steck <me@brysonsteck.xyz>
2024-06-03 22:40:37 -0600

    refactoring POST with snake case

--- backend/internal/server/apis/user/user.go
+++ backend/internal/server/apis/user/user.go
@@ -14,17 +14,9 @@ import (
 	"github.com/jak103/powerplay/internal/server/apis"
 	"github.com/jak103/powerplay/internal/server/services/auth"
 	"github.com/jak103/powerplay/internal/utils/locals"
+	"github.com/jak103/powerplay/internal/utils/log"
 	"github.com/jak103/powerplay/internal/utils/responder"
 )
-
-type createRequest struct {
-	FirstName  string `json:"firstName"`
-	LastName   string `json:"lastName"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
-	Phone      string `json:"phone"`
-	SkillLevel int    `json:"skillLevel"`
-}
 
 type createResponse struct {
 	Email  string `json:"email"`
@@ -42,12 +34,13 @@ func removeFormat(str string) string {
 	return regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(str, "")
 }
 
-func validateUser(u *createRequest) error {
+func validateUser(u *models.User) error {
 
 	//Check data field has been filled for all values
 	values := reflect.ValueOf(*u)
 	for i := 0; i < values.NumField(); i++ {
 		v := values.Field(i).String()
+		log.Info(v)
 		if v == "" {
 			return errors.New("data field is empty")
 		}
@@ -77,15 +70,35 @@ func createUserAccount(c *fiber.Ctx) error {
 
 	// verify the request
 	log := locals.Logger(c)
-	creds := createRequest{}
+
+	// anonymous struct to hold the request data
+	creds := struct {
+		FirstName  string `json:"first_name"`
+		LastName   string `json:"last_name"`
+		Email      string `json:"email"`
+		Password   string `json:"password"`
+		Phone      string `json:"phone"`
+		SkillLevel int    `json:"skill_level"`
+	}{}
+
 	err := c.BodyParser(&creds)
 	if err != nil {
 		log.WithErr(err).Error("Failed to parse user creation request")
 		return responder.BadRequest(c, "Failed to parse user creation request")
 	}
 
+	// marshal the request data into a User struct
+	user := models.User{
+		FirstName:  creds.FirstName,
+		LastName:   creds.LastName,
+		Email:      creds.Email,
+		Password:   creds.Password,
+		Phone:      creds.Phone,
+		SkillLevel: creds.SkillLevel,
+	}
+
 	// validate the request
-	err = validateUser(&creds)
+	err = validateUser(&user)
 	if err != nil {
 		log.WithErr(err).Error(err.Error())
 		return responder.BadRequest(c, err.Error())
@@ -101,7 +114,7 @@ func createUserAccount(c *fiber.Ctx) error {
 
 	// write to database
 	db := db.GetSession(c)
-	log.Debug("Creating user %s", creds.Email)
+	log.Debug("Creating user %s", user.Email)
 	u, result := db.CreateUser(u)
 	if result != nil {
 		log.WithErr(err).Error(result.Error())
@@ -110,7 +123,7 @@ func createUserAccount(c *fiber.Ctx) error {
 
 	// response
 	createdUserResponse := createResponse{
-		Email:  creds.Email,
+		Email:  user.Email,
 		UserId: int(u.ID),
 	}
 
