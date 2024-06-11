3de29a22a25e38a233baf519f0eb05287f8b224f
epeterson42 <a02075248@usu.edu>
2024-06-10 22:13:20 -0600

    Fix GetLeaguesBySeason to also include teams

--- backend/internal/db/league.go
+++ backend/internal/db/league.go
@@ -12,6 +12,19 @@ func (s Session) GetLeagues() ([]models.League, error) {
 func (s Session) GetLeaguesBySeason(seasonId uint) ([]models.League, error) {
 	leagues := make([]models.League, 0)
 	err := s.Connection.Where(&models.League{SeasonID: seasonId}).Find(&leagues)
+
+	for index, league := range leagues {
+		teams := make([]models.Team, 0)
+
+		// db.Find(&users, User{Age: 20})
+		teamErr := s.Connection.Find(&teams, models.Team{LeagueID: league.ID})
+		if teamErr != nil {
+			// Do nothing this will just add an empty list
+		}
+
+		leagues[index].Teams = teams
+	}
+
 	return resultsOrError(leagues, err)
 }
 
