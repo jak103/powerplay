e365ed063b831003d2b592e3a001bfe374f5a05f
Aryan Osqueezadeh <A02289878@aggies.usu.edu>
2024-06-11 12:51:28 -0600

    Updated information index styling

--- site/src/pages/information/InformationPage.vue
+++ site/src/pages/information/InformationPage.vue
@@ -1,16 +1,16 @@
 <template>
   <q-page padding>
     <div class="container py-4">
-      <h1 class="primary">Looking to Participate?</h1>
+      <h1 class="text-primary text-weight-regular">Looking to Participate?</h1>
 
-      <p>
+      <p class="text-body1">
         Looking to participate in a sport that requires skill, strength, 
         strategic thinking and the desire to play as part of a team? Whatever 
         level you play at, hockey can enhance your life, improving fitness, 
         strength and coordination.
       </p>
 
-      <p>
+      <p class="text-body1">
         Whether you are looking to play casually in our recreational league 
         or competitively with one of our traveling teams, you'll find our 
         organization to be a great place to learn new skills and meet new friends.
