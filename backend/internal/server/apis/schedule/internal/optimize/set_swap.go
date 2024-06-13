079002f5b1be224a68af23d6dd2c4f099cb56b22
popska <popska@tuta.io>
2024-06-12 21:05:54 -0600

    Clean up the new algorithm

--- backend/internal/server/apis/schedule/internal/optimize/set_swap.go
+++ backend/internal/server/apis/schedule/internal/optimize/set_swap.go
@@ -21,20 +21,23 @@ func SetOptimizeSchedule(games []models.Game) {
 }
 
 func setSwapSchedule(games []models.Game, seasonStats structures.SeasonStats, teamStats map[string]structures.TeamStats) {
-	teams := make([]string, 0)
-	for key := range teamStats {
-		teams = append(teams, key)
+	var leagues map[string]bool // set of leagues
+	for _, team := range teamStats {
+		if _, ok := leagues[team.League]; !ok {
+			leagues[team.League] = true
+		}
 	}
-	sort.Strings(teams)
+	teamsPerLeague := len(teamStats) / len(leagues)
 
-	for i := 0; i < 20; i++ { // FIXME hardcoded 20 for number of "league sets"
-		relevant_games := games[i : i+6]
-		worst_imbalance := -1
-		worst_index := -1
-		var worst_stats structures.TeamStats
-		for j := 0; j < 6; j++ {
-			team0 := relevant_games[j].HomeTeam.Name
-			team1 := relevant_games[j].AwayTeam.Name
+	// This algorithm is based on taking sets of games and trying to swap games for teams with inverse imbalances
+	for i := 0; i < len(games)/teamsPerLeague; i++ {
+		relevantGames := games[i : i+teamsPerLeague]
+		worstImbalance := -1
+		worstIndex := -1
+		var worstStats structures.TeamStats
+		for j := 0; j < teamsPerLeague; j++ {
+			team0 := relevantGames[j].HomeTeam.Name
+			team1 := relevantGames[j].AwayTeam.Name
 			team0stats := teamStats[team0]
 			team1stats := teamStats[team1]
 
@@ -42,67 +45,67 @@ func setSwapSchedule(games []models.Game, seasonStats structures.SeasonStats, te
 			if team0delta < 0 {
 				team0delta *= -1
 			}
-			if team0delta > worst_imbalance {
-				worst_stats = team0stats
-				worst_imbalance = team0delta
-				worst_index = i*6 + j
+			if team0delta > worstImbalance {
+				worstStats = team0stats
+				worstImbalance = team0delta
+				worstIndex = i*teamsPerLeague + j
 			}
 
 			team1delta := team1stats.EarlyGames - team1stats.LateGames
 			if team1delta < 0 {
 				team1delta *= -1
 			}
-			if team1delta > worst_imbalance {
-				worst_stats = team1stats
-				worst_imbalance = team1delta
-				worst_index = i*6 + j
+			if team1delta > worstImbalance {
+				worstStats = team1stats
+				worstImbalance = team1delta
+				worstIndex = i*teamsPerLeague + j
 			}
 		}
 
 		// if all teams are balanced already don't do anything
-		if worst_imbalance <= 2 {
+		if worstImbalance <= 2 {
 			continue
 		}
 
 		// now that we've found the team with the worst stats this iteration, we try to find the team
 		// with the most opposite imbalance to swap with
-		more_early := worst_stats.EarlyGames > worst_stats.LateGames
-		opposite_imbalance := -1
-		opposite_index := -1
-		for j := 0; j < 6; j++ {
-			team0 := relevant_games[j].HomeTeam.Name
-			team1 := relevant_games[j].AwayTeam.Name
+		moreEarly := worstStats.EarlyGames > worstStats.LateGames
+		oppositeImbalance := -1
+		oppositeIndex := -1
+		for j := 0; j < teamsPerLeague; j++ {
+			team0 := relevantGames[j].HomeTeam.Name
+			team1 := relevantGames[j].AwayTeam.Name
 			team0stats := teamStats[team0]
 			team1stats := teamStats[team1]
 
-			if more_early {
+			if moreEarly {
 				team0delta := team0stats.LateGames - team0stats.EarlyGames
-				if team0delta > opposite_imbalance {
-					opposite_imbalance = team0delta
-					opposite_index = i*6 + j
+				if team0delta > oppositeImbalance {
+					oppositeImbalance = team0delta
+					oppositeIndex = i*teamsPerLeague + j
 				}
 
 				team1delta := team1stats.LateGames - team1stats.EarlyGames
-				if team1delta > opposite_imbalance {
-					opposite_imbalance = team1delta
-					opposite_index = i*6 + j
+				if team1delta > oppositeImbalance {
+					oppositeImbalance = team1delta
+					oppositeIndex = i*teamsPerLeague + j
 				}
 			} else {
 				team0delta := team0stats.EarlyGames - team0stats.LateGames
-				if team0delta > opposite_imbalance {
-					opposite_imbalance = team0delta
-					opposite_index = i*6 + j
+				if team0delta > oppositeImbalance {
+					oppositeImbalance = team0delta
+					oppositeIndex = i*teamsPerLeague + j
 				}
 
 				team1delta := team1stats.EarlyGames - team1stats.LateGames
-				if team1delta > opposite_imbalance {
-					opposite_imbalance = team1delta
-					opposite_index = i*6 + j
+				if team1delta > oppositeImbalance {
+					oppositeImbalance = team1delta
+					oppositeIndex = i*teamsPerLeague + j
 				}
 			}
 		}
 
-		updateStats(teamStats, games, worst_index, opposite_index)
-		swapGames(games, worst_index, opposite_index)
+		updateStats(teamStats, games, worstIndex, oppositeIndex)
+		swapGames(games, worstIndex, oppositeIndex)
 	}
 }
