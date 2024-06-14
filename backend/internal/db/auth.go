bef9dacaaad97e895cb5648107e0c0a8030b2448
Jacob Christensen <jak103@users.noreply.github.com>
2024-06-13 19:09:58 -0600

    In class stuff (#141)
    * In class stuff
    
    * some fixes
    
    * Fix Docker.production and a build error
    
    ---------
    
    Co-authored-by: Nate Stott <nate.stott@pm.me>

--- backend/internal/db/auth.go
+++ backend/internal/db/auth.go
@@ -2,10 +2,10 @@ package db
 
 import "github.com/jak103/powerplay/internal/models"
 
-func (s *session) GetUserByUsername(username string) (*models.User, error) {
+func (s *session) GetUserByEmail(email string) (*models.User, error) {
 	user := &models.User{}
 
-	result := s.Where("username = ?", username).First(user)
+	result := s.Where("email = ?", email).First(user)
 
 	return resultOrError(user, result)
 }
